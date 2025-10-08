a7812d52758f9be9a9e597800759c7a7f85dbcb8
Joseph Bacha <jobachx31@gmail.com>
2025-10-08 09:44:52 +0000

    add light theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Todo from "./components/Todo";
 function App() {
   return (
-    <div className=" bg-neutral-800 grid min-h-screen text-white font-inter">
+    <div className=" bg-neutral-300 dark:bg-neutral-800 grid min-h-screen text-white font-inter">
       <Todo />
     </div>
   );
